8babe5f1a8ccb00c5543f03f0df8e8c112a8b70a
Trung Tin <buitrungtin0105@gmail.com>
2024-10-26 13:29:09 +0000

    handle types of upload files

--- app/utils/load_doc.py
+++ app/utils/load_doc.py
@@ -7,22 +7,26 @@ from typing import List
 vector_store = vector.Vectordb()
 def load(file: UploadFile):
     documents = []
-    try:
-        # Extract content
-        pdf_reader = PdfReader(file.file)
-        for page_num, page in enumerate(pdf_reader.pages):
-            text = page.extract_text()
-            document = Document(
-                page_content=text,
-                metadata={
-                    "filename": file.filename,
-                    "page_number": page_num
-                }
-            )
-            documents.append(document)
+    if file.filename.endswith('.pdf'):
+        try:
+            # Extract content
+            pdf_reader = PdfReader(file.file)
+            for page_num, page in enumerate(pdf_reader.pages):
+                text = page.extract_text()
+                document = Document(
+                    page_content=text,
+                    metadata={
+                        "filename": file.filename,
+                        "page_number": page_num
+                    }
+                )
+                documents.append(document)
 
-    except Exception as e:
-        print(f"Error loading file {file.filename}: {e}")
-
+        except Exception as e:
+            print(f"Error loading file {file.filename}: {e}")
+    elif file.filename.endswith('.txt'):
+        pass
+    else:
+        print(f"Unsupported file format: {file.filename}")
 # Add all documents in batch
     vector_store.add_doc(documents)
