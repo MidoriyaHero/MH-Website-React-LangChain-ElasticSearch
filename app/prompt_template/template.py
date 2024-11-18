befbb0218a2d26d1ebe4ce27f696ce5d02776ed6
MidoriyaHero <buitrungtin0105@gmail.com>
2024-11-18 20:15:12 +0700

    add some prompts

--- app/prompt_template/template.py
+++ app/prompt_template/template.py
@@ -14,5 +14,20 @@ Only return the final standalone question. \
 """
 
 #todo create prompt for chat history
+prompt_history = """
+Here is conservation 20 messages between AI and Human, your task is to summarize this conservation into paragraphs \
+with 250 words. 1 paragraph for  AI messages, 1 paragraph for Human messages.
+{messages_history}
+"""
 #todo create prompt for evaluate journal
+journal_evaluate_prompt = """
+Base on the checklist {checklist}, your task is to evaluate these daily journals of patient. Focus on:\
+
+
+"""
 #todo create prompt for evaluate response
+response_evaluate_prompt = """
+you are an expert in bioethic. Your task is to evaluate this response base on bioethic aspects and mental health aspect\
+{response}
+
+"""
