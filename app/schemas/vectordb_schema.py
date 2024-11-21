6669bb85ce9175380dd52b99c84def7eaeee6fa2
MidoriyaHero <buitrungtin0105@gmail.com>
2024-11-21 22:36:04 +0700

    add vectordb - chat history schema

--- app/schemas/vectordb_schema.py
+++ app/schemas/vectordb_schema.py
@@ -1,1 +1,12 @@
 #todo add a shema for history chat services
+from pydantic import BaseModel, Field
+from datetime import datetime 
+from typing import List
+
+class Message(BaseModel):
+    role: str = Field(..., title = "Role")
+    content: str = Field(..., title = "Content")
+    create_at: datetime = Field(default_factory = datetime.now)
+
+class History_chat(BaseModel):
+    history: List[Message]
