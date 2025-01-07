f6c8006e14f66aa0f5382e38a61ec08b16906c11
Trung Tin <buitrungtin0105@gmail.com>
2025-01-07 20:02:20 +0700

    add package file

--- app/main.py
+++ app/main.py
@@ -31,7 +31,7 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = 'http://localhost:3000',
+    allow_origins = 'http://13.236.193.72',
     allow_credentials = True,
     allow_methods = ['*'],
     allow_headers = ['*']
